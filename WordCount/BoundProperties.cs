15b3a8d1b087cc96a593da79fa334fecfd9bba03
Aron Wiley <61031954+ajwiley@users.noreply.github.com>
2021-05-03 14:44:13 -0500

    Re-done in C#/wpf

--- WordCount/BoundProperties.cs
+++ WordCount/BoundProperties.cs
@@ -1,27 +1,51 @@
-using System;
-using System.ComponentModel;
-using System.Text.RegularExpressions;
-using JetBrains.Annotations;
+﻿using System.ComponentModel;
 
 namespace WordCount {
     public class BoundProperties : INotifyPropertyChanged {
-        private string _TextContent;
-        public string TextContent {
-            get => _TextContent;
+        private string _Text;
+        public string Text {
+            get => _Text;
             set {
-                _TextContent = value;
-                OnPropertyChanged(nameof(TextContent));
+                _Text = value;
+                NumberWords = Functions.NumberOfWords(_Text); // Get the number of words in the text
+                NumberChars = _Text.Length;
+                OnPropertyChanged(nameof(Text));
             }
         }
 
-        public BoundProperties() {
-            TextContent = "";
+        private int _NumberWords;
+        public int NumberWords {
+            get => _NumberWords;
+            set {
+                _NumberWords = value;
+                OnPropertyChanged(nameof(NumberWords));
+            }
         }
 
-        public event PropertyChangedEventHandler? PropertyChanged;
+        private int _NumberChars;
+        public int NumberChars {
+            get => _NumberChars;
+            set {
+                _NumberChars = value;
+                OnPropertyChanged(nameof(NumberChars));
+            }
+        }
 
-        private void OnPropertyChanged(string propName) {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+
+        // These are the default values
+        public BoundProperties() {
+            NumberWords = 0;
+            NumberChars = 0;
+        }
+
+        // Delegates can be used in event handling to pass values to the UI thread.
+        // To implement the INotifyPropertyChanged interface, you must register the PropertyChangedEventHandler delegate as an event.
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // When the PropertyChanged event is raised, this method will instantiate an object containing the name of the property that was changed 
+        // so the UI control can connect to the appropriate property.
+        protected void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
---
+++ WordCount/Functions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace WordCount {
+    static class Functions {
+        public static int NumberOfWords(string para) {
+            Regex reg = new Regex("[^a-zA-Z]+");
+
+            int num = 1;
+            char lastValue = ' ';
+
+            // Find all the places where there is a space, tab or newline, these mean new words
+            foreach (char c in para) {
+                if (c == ' ' || c == '\t' || c == '\n') {
+                    num += 1;
+                }
+                else if (reg.IsMatch(c.ToString()) && lastValue == ' ') { // Words must be valid (aka only a-zA-Z), but only if there is a space afterwards
+                    num -= 1;
+                }
+
+                lastValue = c;
+            }
+
+            // Don't add another word if the string just ends with a space, tab or new line.
+            if (para.EndsWith(' ') || para.EndsWith('\t') || para.EndsWith('\n')) {
+                num -= 1;
+            }
+
+            // Nothing is in the string
+            if (para.Length < 1) {
+                num = 0;
+            }
+
+            // If the user has one char that and it is a space, tab, or newline, set the count to 0.
+            if (para.Length == 1 && para.Contains(' ') || para.Contains('\t') || para.Contains('\n')) {
+                num = 0;
+            }
+            
+            return num;
+        }
+    }
+}
